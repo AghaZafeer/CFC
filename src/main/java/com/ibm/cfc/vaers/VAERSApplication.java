bdbff7a0b7cd09d347e4d5fb1cdad50d9a26270a
02366B744 <02366B744@9.79.179.37>
2021-07-14 23:07:22 +0530

    Initial Commit of Project Structure

---
+++ src/main/java/com/ibm/cfc/vaers/VAERSApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.cfc.vaers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VAERSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VAERSApplication.class, args);
+	}
+
+}
